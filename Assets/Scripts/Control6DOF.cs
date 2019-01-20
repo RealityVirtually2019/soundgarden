6bd0894ff8df56ae7b4c4bf0f05d1f15d32cee01
Dan Hart <daniel_hart@wgbh.org>
2019-01-20 10:01:38 -0500

    consistent gameobject on the controller, set 1a

--- Assets/Scripts/Control6DOF.cs
+++ Assets/Scripts/Control6DOF.cs
@@ -1,19 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.XR.MagicLeap;
+
 
 public class Control6DOF : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    #region Private Variables
+    private MLInputController _controller;
+    #endregion
 
-        // todo: paste the code from the Unity 6DOF tutorial here!
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    #region Unity Methods
+    void Start()
+    {
+        //Start receiving input by the Control
+        MLInput.Start();
+        _controller = MLInput.GetController(MLInput.Hand.Left);
+    }
+    void OnDestroy()
+    {
+        //Stop receiving input by the Control
+        MLInput.Stop();
+    }
+    void Update()
+    {
+        //Attach the Beam GameObject to the Control
+        transform.position = _controller.Position;
+        transform.rotation = _controller.Orientation;
 
         // todo: in addition to the code from the tutorial, need to add the collision
 
@@ -21,12 +34,7 @@ public class Control6DOF : MonoBehaviour {
         // on collision enter
         // other.find component<audioplayer>.play()
         // other.find soundanimation.play
-		
-	}
-
-    void Load_a_Scene()
-    {
-        SceneManager.LoadSceneAsync(1);
-        // that should do it!
     }
+    #endregion
+
 }
