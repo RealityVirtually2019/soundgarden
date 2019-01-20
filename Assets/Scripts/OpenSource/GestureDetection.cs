6104fb1ad29770e65fdd38a6d61e034959c8a3b0
Dan Hart <daniel_hart@wgbh.org>
2019-01-19 20:21:26 -0500

    hand gesture tracking and tracking

---
+++ Assets/Scripts/OpenSource/GestureDetection.cs
@@ -0,0 +1,87 @@
+﻿/*
+ * Script: GestureDetection.cs
+ * Author: King
+ * Site: http://codeofcodehall.co.uk
+ * Date: May 6th 2018
+ * Lumin SDK: 0.13.0
+ * Desc: Setup the gestures we want to track.
+ *       When the gesture is detected update 
+ *       the position of the hand.
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.MagicLeap;
+public class GestureDetection : MonoBehaviour
+{
+    //game object representing the finger position
+    public Transform finger;
+    // Use this for initialization
+    void Start()
+    {
+        MLHands.Start();
+        if (!MLHands.IsStarted)
+        {
+            Debug.Log("MLHands didn't start...bail.");
+            return;
+        }
+        // set gestures to track
+        setGesturesToTrack();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if MLHands is running start tracking gestures
+        if (MLHands.IsStarted)
+            gestureTracker();
+    }
+
+    //set the gestures to track
+    void setGesturesToTrack()
+    {
+        List<MLHandKeyPose> gestures = new List<MLHandKeyPose>();
+        //add the gestures we want to track
+        gestures.Add(MLHandKeyPose.Finger);
+        gestures.Add(MLHandKeyPose.L);
+        //add the gestures to the gesture manager
+        MLHands.KeyPoseManager.EnableKeyPoses(gestures.ToArray(), true, true);
+    }
+
+    //track the gestures
+    void gestureTracker()
+    {
+        //check for the l or finger gesture
+        //and that we've got some keypoints
+        if ((MLHands.Left.KeyPose == MLHandKeyPose.Finger ||
+               MLHands.Left.KeyPose == MLHandKeyPose.L))
+        {
+            positionHand(MLHands.Left);
+        }
+        //check for the l or finger gesture
+        //and that we've got some keypoints
+        if ((MLHands.Right.KeyPose == MLHandKeyPose.Finger ||
+                MLHands.Right.KeyPose == MLHandKeyPose.L))
+        {
+            positionHand(MLHands.Right);
+        }
+    }
+
+    //position the game object to the tracked finger
+    void positionHand(MLHand hand)
+    {
+        //in Lumin SDK 0.13 only the center hand position
+        //updates so we need to use that until it's fixed
+        //finger.position = hand.Index.Tip.Position;
+        finger.position = hand.Center;
+        //set the finger position to the tip of the finger
+        //finger.position = hand[1].position;
+        Debug.Log("Hand position Z " + hand.Center.z + " / " + finger.position + " finger position" );
+    }
+
+    private void OnDestroy()
+    {
+        MLHands.Stop();
+    }
+}
