8eb6f07bb72cd026d92b618877e54049656aa05f
Dan Hart <daniel_hart@wgbh.org>
2019-01-20 12:03:19 -0500

    end of testing for capabilities on sounds

--- Assets/Scripts/Control6DOF.cs
+++ Assets/Scripts/Control6DOF.cs
@@ -13,14 +13,28 @@ public class Control6DOF : MonoBehaviour {
     #region Public Variables
     public bool AllowEnter = true;
     public bool AllowExit = true;
+
+    // todo: could put the four color GO's here and then I don't need to look for them
+    public GameObject Yellow;
+    public GameObject Blue;
+    public GameObject Pink;
+    public GameObject Green;
     #endregion
 
     #region Unity Methods
+    void resetMoodVisuals()
+    {
+        Blue.SetActive(false);
+        Yellow.SetActive(false);
+        Pink.SetActive(false);
+        Green.SetActive(false);
+    }
     void Start()
     {
         //Start receiving input by the Control
         MLInput.Start();
         _controller = MLInput.GetController(MLInput.Hand.Left);
+        resetMoodVisuals();
     }
     void OnDestroy()
     {
@@ -47,10 +61,44 @@ public class Control6DOF : MonoBehaviour {
     {
         if (AllowEnter)
         {
-            // todo: check for an audio source and play it!
-            Debug.Log("entered");
+            Debug.Log("entered the " + other.name);
+            if (other.GetComponent<AudioSource>())
+            {
+                try
+                {
+                    other.GetComponent<AudioSource>().Play();
+                }
+                catch
+                {
+                    Debug.Log("no audio clip");
+                }
+            }
+            string myname = other.name;
+            if (myname.Contains("Yellow"))
+            {
+                resetMoodVisuals();
+                Yellow.SetActive(true);
+                // todo: loop through children and start animations!
+            }
+            else if (myname.Contains("Blue"))
+            {
+                resetMoodVisuals();
+                Blue.SetActive(true);
+            }
+            else if (myname.Contains("Pink"))
+            {
+                resetMoodVisuals();
+                Pink.SetActive(true);
+            }
+            else if (myname.Contains("Green"))
+            {
+                resetMoodVisuals();
+                Green.SetActive(true);
+            }
         }
+
     }
+
     private void OnTriggerExit(Collider other)
     {
         if (AllowExit)
